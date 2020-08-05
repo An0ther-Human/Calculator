fd9879696d292147864134164063c63dbfca029d
C0ldHands <leeyuenhigh@gmail.com>
2020-08-04 21:22:03 -0400

    Keyboard support try 1

--- script.js
+++ script.js
@@ -37,7 +37,11 @@ function operate(num1, num2, operator){
 const buttons = document.querySelectorAll('button');
 for (i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', isOperator);
+    // buttons[i].addEventListener('keydown', isOperator);
 }
+window.addEventListener('keydown', isOperator);
+
+
 const input = document.querySelector('#input');
 const ans = document.querySelector('#ans')
 let userInput = '';
@@ -46,10 +50,28 @@ let noMultiDivide = [];
 let temp;
 let text;
 let useCounter = 0;
+const validKeycodes = [55, 56, 57, 191, 52, 53, 54, 56, 49, 50, 51, 189, 48, 190, 65, 187, 67, 8, 69]
 function isOperator(e){
-    if (!useCounter >= 1){
-        console.log(userInput)
-        if (e.target.textContent == '.'){
+    console.log(e.type)
+    let content = e.target.textContent;
+    let has2Class = !e.target.classList.contains('noDisplay') && !e.target.classList.contains('operator');
+    let hasClass = !e.target.classList.contains('noDisplay');
+    let target = e.target;
+
+    if (e.type != 'click'){
+        if (validKeycodes.includes(e.keyCode)){
+            let pressedBtn = document.querySelector(`button[data-key="${e.keyCode}"]`);
+            content = pressedBtn.textContent;
+            has2Class = !pressedBtn.classList.contains('operator') && !pressedBtn.classList.contains('noDisplay');
+            hasClass = !pressedBtn.classList.contains('noDisplay');
+            target = pressedBtn;
+        }else{
+            return;
+        } 
+    }
+    console.log(content)
+    if (content == '.'){
+        if (!useCounter >= 1){
             if (input.textContent.length == 0){
                 userInput = '0.'
                 input.textContent = userInput;
@@ -65,21 +87,27 @@ function isOperator(e){
                     }
                 }
             }
-        }else if (!e.target.classList.contains('noDisplay') && !e.target.classList.contains('operator')){
-                userInput += e.target.textContent;
-                input.textContent = userInput;
-        }else if (!e.target.classList.contains('noDisplay')){
+        }
+    }else if (has2Class){
+        if (!useCounter >= 1){
+            userInput += content;
+            input.textContent = userInput;
+        }
+    }else if (hasClass){
+        if (!useCounter >= 1){
             if (input.textContent.length != 0){
                 if (userInput.charAt(userInput.length-1) != ' '){
-                    userInput += ` ${e.target.textContent} `;
+                    userInput += ` ${content} `;
                     input.textContent = userInput;
                 }else{
                     userInput = userInput.slice(0,userInput.length-3);
-                    userInput += ` ${e.target.textContent} `;
+                    userInput += ` ${content} `;
                     input.textContent = userInput;
                 }
             }
-        }else if(e.target.id == 'equal'){
+        }
+    }else if(target.id == 'equal'){
+        if (!useCounter >= 1){
             text = userInput.split(' ')
             let start;
             let previousValue;
@@ -102,7 +130,6 @@ function isOperator(e){
                 }
             }
             start = Number(noMultiDivide[0]);
-            console.log(noMultiDivide)
             if (!noMultiDivide.includes('Calculation failed.')){
                 for(i =0; i<noMultiDivide.length; i++){
                     if (noMultiDivide.includes('+') || noMultiDivide.includes('-')){
@@ -119,7 +146,9 @@ function isOperator(e){
             }
             ans.textContent = answer;
             useCounter++
-        }else if(e.target.id == 'backspace'){
+        }
+    }else if(target.id == 'backspace'){
+        if (!useCounter >= 1){
             if (input.textContent.charAt(input.textContent.length -1) == ' '){
                 input.textContent = input.textContent.slice(0,-3);
                 userInput = userInput.slice(0,-3);
@@ -128,31 +157,59 @@ function isOperator(e){
                 userInput = userInput.slice(0,-1);
             }
         }
-    }
-}
-
-const ansBtn = document.querySelector('#ansBtn');
-ansBtn.addEventListener('click', operateOnPreviousAns)
-
-function operateOnPreviousAns(e){
-    if (useCounter != 0){
+    }else if (target.id == 'ansBtn'){
+        if (useCounter != 0){
+            ans.textContent = null;
+            noMultiDivide = [];
+            userInput = answer.toString();
+            input.textContent = userInput;
+            useCounter = 0;
+        }
+    }else if(target.id == 'clear'){
+        userInput = '';
+        input.textContent = '';
         ans.textContent = null;
         noMultiDivide = [];
-        userInput = answer.toString();
-        input.textContent = userInput;
+        temp = null;
+        text = null;
         useCounter = 0;
     }
 }
 
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', resetCalculator)
+// const ansBtn = document.querySelector('#ansBtn');
+// ansBtn.addEventListener('click', operateOnPreviousAns)
+// window.addEventListener('keydown', operateOnPreviousAns);
 
-function resetCalculator(e){
-    userInput = '';
-    input.textContent = '';
-    ans.textContent = null;
-    noMultiDivide = [];
-    temp = null;
-    text = null;
-    useCounter = 0;
+// function operateOnPreviousAns(e){
+//     if (useCounter != 0){
+//         ans.textContent = null;
+//         noMultiDivide = [];
+//         userInput = answer.toString();
+//         input.textContent = userInput;
+//         useCounter = 0;
+//     }
+// }
+
+// const clear = document.querySelector('#clear');
+// clear.addEventListener('click', resetCalculator)
+
+// function resetCalculator(e){
+//     userInput = '';
+//     input.textContent = '';
+//     ans.textContent = null;
+//     noMultiDivide = [];
+//     temp = null;
+//     text = null;
+//     useCounter = 0;
+// }
+
+
+function keyBorardOperation(e){
+    let pressedBtn = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    console.log(pressedBtn);
+    console.log(pressedBtn.textContent);
+    console.log(!pressedBtn.classList.contains('operator') && !pressedBtn.classList.contains('nodisplay'))
+    console.log(!pressedBtn.classList.contains('noDisplay'))
+    console.log(pressedBtn.id)
+    // console.log(pressedBtn.type)
 }
